712637786eab1e1033f8690f2e905d5e2ee97f96
Nguyen Phu <nvphu1306@gmail.com>
2018-06-18 10:50:08 +0700

    fix train.py

--- train.py
+++ train.py
@@ -22,19 +22,19 @@ while(1):
     e1 = re.search(r'<e1>(.*)</e1>', data).group(1)
     e2 = re.search(r'<e2>(.*)</e2>', data).group(1)
     label = file.readline().replace("\n", "")
-    try:
-        e = re.search(r'(.*)(\(.*\))', label).groups(2)
-    except AttributeError:
-        e = ['', 'other']
-    if e[1] == '(e2,e1)':
-        words.append([e2, e1])
-    else:
-        words.append([e1, e2])
-    #words.append([e1,e2])
+    # try:
+    #     e = re.search(r'(.*)(\(.*\))', label).groups(2)
+    # except AttributeError:
+    #     e = ['', 'other']
+    # if e[1] == '(e2,e1)':
+    #     words.append([e2, e1])
+    # else:
+    #     words.append([e1, e2])
+    words.append([e1,e2])
     comment = file.readline()
     blank = file.readline()
     # labels.append(label)
-    labels.append(label.replace("(e1,e2)", "").replace("(e2,e1)", ""))
+    labels.append(label)
 file.close()
 print("loaded data ... ")
 
@@ -44,7 +44,7 @@ label_list = list(filtered_sentence.keys())
 model = Word2Vec.load('./train.w2v')
 
 
-X_train = np.zeros((8000, 400))
+X_train = np.zeros((8000, 200))
 y_train = np.zeros(8000)
 for i in range(8000):
     a = model[words[i][0]]
@@ -61,11 +61,8 @@ for i in range(8000):
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=500)
 print('training...')
 alpha = 1e-1 # regularization parameter
-clf = MLPClassifier(activation='tanh', solver='sgd', alpha=alpha, hidden_layer_sizes=(1000))
+clf = MLPClassifier(activation='tanh', solver='sgd', alpha=alpha, hidden_layer_sizes=(200))
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 acc = 100*np.mean(y_pred == y_test)
 print('training accuracy: %.2f %%' % acc)
-
-
-
